49f58f71694a3c7db0d68fecc3bec3ab1ba4279e
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-21 04:01:53 +0900

    【セクション2】13. Chrome Notifications API
    On branch udemy-ReactTS-ChromeExtension-section2
    Changes to be committed:
    	modified:   ReactTS-ChromeExtension-section2/background.js
    	modified:   ReactTS-ChromeExtension-section2/manifest.json

--- ReactTS-ChromeExtension-section2/background.js
+++ ReactTS-ChromeExtension-section2/background.js
@@ -7,5 +7,14 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     const time = res.timer ?? 0;
     chrome.storage.local.set({ timer: time + 1 });
     chrome.action.setBadgeText({ text: `${time + 1}` });
+
+    if (time % 1000 === 0) {
+      this.registration.showNotification('Chrome Timer Extension', {
+        body: '1 second has passed!',
+        icon: 'icon.png',
+      });
+    }
   });
 });
+
+console.log(this);
