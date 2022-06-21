21c0a8ee64b3204441f24b5dc9b325fe966d16bb
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-21 19:21:15 +0900

    【セクション3】20. Tasks List Feature Part 2
    On branch udemy-ReactTS-ChromeExtension-section3
    Changes to be committed:
    	modified:   ReactTS-ChromeExtension-section3/manifest.json
    	modified:   ReactTS-ChromeExtension-section3/popup/popup.js

--- ReactTS-ChromeExtension-section3/popup/popup.js
+++ ReactTS-ChromeExtension-section3/popup/popup.js
@@ -1,27 +1,34 @@
-const tasks = [];
+let tasks = [];
 
 const addTaskBtn = document.getElementById('add-task-btn');
 addTaskBtn.addEventListener('click', () => addTask());
 
-function addTask() {
-  const taskNum = tasks.length;
-  tasks.push('');
+chrome.storage.sync.get(['tasks'], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
+function saveTasks() {
+  chrome.storage.sync.set({ tasks });
+}
 
+function renderTask(taskNum) {
   const taskRow = document.createElement('div');
 
   const text = document.createElement('input');
   text.type = 'text';
   text.placeholder = 'Enter a task...';
+  text.value = tasks[taskNum];
   text.addEventListener('change', () => {
     tasks[taskNum] = text.value;
     console.log(tasks);
+    saveTasks();
   });
 
   const deleteBtn = document.createElement('input');
   deleteBtn.type = 'button';
   deleteBtn.value = 'X';
   deleteBtn.addEventListener('click', () => {
-    tasks.splice(taskNum, 1);
+    deleteTask(taskNum);
   });
 
   taskRow.appendChild(text);
@@ -29,4 +36,25 @@ function addTask() {
 
   const taskContainer = document.getElementById('task-container');
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const taskNum = tasks.length;
+  tasks.push('');
+  renderTask(taskNum);
+  saveTasks();
+}
+
+function deleteTask(taskNum) {
+  tasks.splice(taskNum, 1);
+  renderTasks();
+  saveTasks();
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById('task-container');
+  taskContainer.textContent = '';
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
 }
