9974b30ee91222c096e6258684aaebfc4257903e
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-21 20:17:12 +0900

    【セクション3】22. Timer Feature Part 2
    On branch udemy-ReactTS-ChromeExtension-section3
    Changes to be committed:
    	modified:   ReactTS-ChromeExtension-section3/background.js
    	modified:   ReactTS-ChromeExtension-section3/popup/popup.html
    	modified:   ReactTS-ChromeExtension-section3/popup/popup.js

--- ReactTS-ChromeExtension-section3/background.js
+++ ReactTS-ChromeExtension-section3/background.js
@@ -7,8 +7,16 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(['timer', 'isRunning'], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
-        console.log(timer);
-        chrome.storage.local.set({ timer });
+        let isRunning = true;
+        if (timer === 60 * 25) {
+          this.registration.showNotification('Pomodoro Timer', {
+            body: '25 minutes has passed!',
+            icon: 'icon.png',
+          });
+          timer = 0;
+          isRunning = false;
+        }
+        chrome.storage.local.set({ timer, isRunning });
       }
     });
   }
