dd497a7ef299655caf7b884dc83173960d83712a
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-21 19:50:24 +0900

    【セクション3】21. Timer Feature Part 1
    On branch udemy-ReactTS-ChromeExtension-section3
    Changes to be committed:
    	new file:   ReactTS-ChromeExtension-section3/background.js
    	modified:   ReactTS-ChromeExtension-section3/manifest.json
    	modified:   ReactTS-ChromeExtension-section3/popup/popup.html
    	modified:   ReactTS-ChromeExtension-section3/popup/popup.js

--- ReactTS-ChromeExtension-section3/popup/popup.js
+++ ReactTS-ChromeExtension-section3/popup/popup.js
@@ -1,5 +1,34 @@
 let tasks = [];
 
+const startTimerBtn = document.getElementById('start-timer-btn');
+startTimerBtn.addEventListener('click', () => {
+  chrome.storage.local.get(['isRunning'], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? 'Pause Timer'
+          : 'Start Timer';
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById('reset-timer-btn');
+resetTimerBtn.addEventListener('click', () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = 'Start Timer';
+    }
+  );
+});
+
 const addTaskBtn = document.getElementById('add-task-btn');
 addTaskBtn.addEventListener('click', () => addTask());
 
