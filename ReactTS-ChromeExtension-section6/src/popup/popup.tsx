08901845c240de046e402f7081926ed878ba2321
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-23 04:02:06 +0900

    【セクション6】49. Open Weather API Part 2
    On branch udemy-ReactTS-ChromeExtension-section6
    Changes to be committed:
    	modified:   src/popup/popup.tsx
    	modified:   src/utils/api.ts

--- ReactTS-ChromeExtension-section6/src/popup/popup.tsx
+++ ReactTS-ChromeExtension-section6/src/popup/popup.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './popup.css'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './popup.css';
+import { fetchOpenWeatherData } from '../utils/api';
 
 const App: React.FC<{}> = () => {
+  useEffect(() => {
+    fetchOpenWeatherData('Toronto')
+      .then((data) => {
+        console.log(data);
+        console.log('Temperature is: ', data.main.temp);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      <img src="icon.png" />
+      <img src='icon.png' />
     </div>
-  )
-}
+  );
+};
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-ReactDOM.render(<App />, root)
+const root = document.createElement('div');
+document.body.appendChild(root);
+ReactDOM.render(<App />, root);
--- ReactTS-ChromeExtension-section6/src/utils/api.ts
+++ ReactTS-ChromeExtension-section6/src/utils/api.ts
@@ -2,15 +2,38 @@
 import { openWeather } from '../../credentials.js';
 const OPEN_WEATHER_API_KEY = openWeather.apiKey;
 
-export async function fetchOpenWeatherData(city: string): Promise<any> {
+export interface OpenWeatherData {
+  name: string;
+  main: {
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
+
+export async function fetchOpenWeatherData(
+  city: string
+): Promise<OpenWeatherData> {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather/?q=${city}&appid=${OPEN_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather/?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
   );
 
   if (!res.ok) {
     throw new Error('City not found');
   }
 
-  const data = await res.json();
+  const data: OpenWeatherData = await res.json();
   return data;
 }
