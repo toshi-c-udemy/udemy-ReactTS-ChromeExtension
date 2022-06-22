c859c27b7dda717d22a99a98b12677e9aa46a6ce
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-22 22:31:25 +0900

    【セクション5】41. Webpack CSS Loaders Part 1
    On branch udemy-ReactTS-ChromeExtension-section5
    Changes to be committed:
    	deleted:    src/manifest.json
    	new file:   src/options/options.tsx
    	renamed:    icon.png -> src/static/icon.png
    	new file:   src/static/manifest.json
    	modified:   webpack.config.js

--- ReactTS-ChromeExtension-section5/webpack.config.js
+++ ReactTS-ChromeExtension-section5/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
   module: {
     rules: [
@@ -21,16 +22,12 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
           to: path.resolve('dist'),
         },
       ],
     }),
-    new HtmlPlugin({
-      title: 'React Extension',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
+    ...getHtmlPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -39,4 +36,20 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
