878f9e18549877b1e5c9f645f0861792b804d32d
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-23 16:55:43 +0900

    【セクション6】53. Popup and Cities Feature Part 1
    On branch udemy-ReactTS-ChromeExtension-section6
    Changes to be committed:
    	modified:   src/popup/popup.css
    	modified:   src/popup/popup.tsx

--- ReactTS-ChromeExtension-section6/src/popup/popup.tsx
+++ ReactTS-ChromeExtension-section6/src/popup/popup.tsx
@@ -1,16 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 import 'fontsource-roboto';
 import './popup.css';
 import WeatherCard from './WeatherCard';
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    'Toronto',
+    'New York',
+    'Error',
+  ]);
+
   return (
-    <div>
-      <WeatherCard city='Toronto' />
-      <WeatherCard city='New York' />
-      <WeatherCard city='Error' />
-    </div>
+    <Box mx={'8px'} my={'16px'}>
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px={'15px'} py={'5px'}>
+              <InputBase placeholder='Add a city name' />
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {cities.map((city, index) => (
+        <WeatherCard city={city} key={index} />
+      ))}
+    </Box>
   );
 };
 
