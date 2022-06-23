298f13d8de234c39728d5e053415c7e860dd74cc
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-24 00:41:16 +0900

    【セクション6】65. Temperature Badge Feature
    On branch udemy-ReactTS-ChromeExtension-section6
    Changes to be committed:
    	modified:   src/background/background.ts
    	modified:   src/static/manifest.json

--- ReactTS-ChromeExtension-section6/src/background/background.ts
+++ ReactTS-ChromeExtension-section6/src/background/background.ts
@@ -1,4 +1,10 @@
-import { setStoredCities, setStoredOptions } from '../utils/storage';
+import {
+  getStoredCities,
+  setStoredCities,
+  getStoredOptions,
+  setStoredOptions,
+} from '../utils/storage';
+import { fetchOpenWeatherData } from '../utils/api';
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([]);
@@ -6,5 +12,36 @@ chrome.runtime.onInstalled.addListener(() => {
     hasAutoOverlay: false,
     homeCity: '',
     tempScale: 'metric',
+  });
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Add city to weather extension',
+    id: 'weatherExtension',
+  });
+
+  chrome.alarms.create({
+    periodInMinutes: 1 / 6,
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    setStoredCities([...cities, event.selectionText]);
+  });
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  getStoredOptions().then((options) => {
+    if (options.homeCity == '') {
+      return;
+    }
+    fetchOpenWeatherData(options.homeCity, options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp);
+      const symbol = options.tempScale === 'metric' ? '\u2103' : '\u2109';
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`,
+      });
+    });
   });
 });
