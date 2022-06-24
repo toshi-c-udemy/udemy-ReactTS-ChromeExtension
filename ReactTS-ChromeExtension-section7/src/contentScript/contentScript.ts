f00d6f317f4ff67d80c901712e748b4b8d90df12
Chida Toshihirro <toshi.c.1986@gmail.com>
2022-06-25 00:12:23 +0900

    【セクション7】72. Content Script DOM Blocking
    On branch udemy-ReactTS-ChromeExtension-section7
    Changes to be committed:
    	modified:   src/contentScript/contentScript.ts
    	modified:   src/static/manifest.json

--- ReactTS-ChromeExtension-section7/src/contentScript/contentScript.ts
+++ ReactTS-ChromeExtension-section7/src/contentScript/contentScript.ts
@@ -1,1 +1,26 @@
-// TODO: content script
+const rules: {
+  [url: string]: () => void;
+} = {
+  'https://www.nytimes.com/section/technology': filterNYTTechnology,
+};
+
+function filterNYTTechnology() {
+  return;
+  const app = document.getElementById('site-content');
+  const wrapper = document.getElementById('top-wrapper');
+  app.removeChild(wrapper);
+}
+
+function filterNYTTechnology2() {
+  const divs = document.getElementsByClassName('div');
+  for (const div of divs) {
+    if (div.className.indexOf('ad') != -1) {
+      div.style.display = 'none';
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log(document.URL);
+  rules[document.URL]();
+}
